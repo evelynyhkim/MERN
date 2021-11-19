d997e69156b28f982ac31049ee99dacdbe52d19f
Evelyn Kim <evelyn.thkim@gmail.com>
2021-11-18 16:33:51 -0800

    useState in a class compoment

--- React/my-app/src/components/PersonCard.js
+++ React/my-app/src/components/PersonCard.js
@@ -1,13 +1,20 @@
 import React, {Component} from 'react'
 
 class PersonCard extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            age: props.age
+        }
+    }
     render(){
         const {firstName, lastName, age, hairColor} = this.props
     
         return (<>
         <h2>{lastName}, {firstName}</h2>
-        <p>Age: {age}</p>
+        <p>Age: {this.state.age}</p>
         <p>Hair Color: {hairColor}</p>
+        <button onClick={e=>this.setState({age: this.state.age+1})}>Birthday has arrived!</button>
     </>)
     }
 }
